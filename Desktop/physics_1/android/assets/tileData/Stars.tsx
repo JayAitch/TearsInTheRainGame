707c367b961f21a4eea54ce72d417ab395f58d4b
JayAitch <jordanjharrison@hotmail.co.uk>
2018-04-13 17:27:27 +0100

    Cars appear to dispawn properly maybe concider pooling this as a resource moving the cars back instead of respawning List is added to rather then created on spawning

---
+++ Desktop/physics_1/android/assets/tileData/Stars.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="starsTexture.jpg" tilewidth="32" tileheight="32" tilecount="350" columns="50">
+ <image source="Tiles/starsTexture.jpg" width="1600" height="245"/>
+</tileset>
